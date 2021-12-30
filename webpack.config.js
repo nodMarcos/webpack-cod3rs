bf2edc5b2bca182858f36ade3c567207ccdd22b3
Marcos <nodmarcos@gmail.com>
2021-12-30 08:59:07 -0300

    more webpack features implemented, finished project

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,49 @@
+const modoDev = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
+const MiniExtract = require('mini-css-extract-plugin');
+const UglifyJsPLugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin =require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPLugin({
+                cache: true,
+                parallel: true,
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new MiniExtract({
+            filename: 'estilo.css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s?[ac]ss$/,
+                use: [
+                    MiniExtract.loader,
+                    //'style-loader',
+                    'css-loader', 
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }
+        ]
     }
 }
